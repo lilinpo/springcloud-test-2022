720ef61edde915716df10a13586e486f0ed44a6c
lilinpo <1319158783@qq.com>
2022-07-25 22:27:34 +0800

    spring-cloud stream 客户端2提交

---
+++ cloud-stream-rabbitmq-consume8803/src/main/java/com/lilinpo/springcloud/RabbitMain8803.java
@@ -0,0 +1,15 @@
+package com.lilinpo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Administrator
+ * @Date 2022/7/25 22:10
+ */
+@SpringBootApplication
+public class RabbitMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMain8803.class,args);
+    }
+}
